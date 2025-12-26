1238c74ad76d4651db874b74c061e0dd43319fa6
GameDev Ali <rekasa1027@gmail.com>
2025-12-26 21:32:27 +0800

    router

---
+++ src/components/Layout.jsx
@@ -0,0 +1,13 @@
+
+import Navigation from './Navigation';
+import Footer from './Footer';
+
+export default function MainLayout({ children }) {
+  return (
+    <>
+      <Navigation />
+      {children}
+      <Footer />
+    </>
+  );
+}
