8557cda8f7bf9a4ca493abbf15dede2e5a0eb0d3
GameDev Ali <rekasa1027@gmail.com>
2025-12-27 12:42:32 +0800

    React Router

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,18 @@ import { Routes, Route, Link } from 'react-router-dom'
 import React from 'react'
 import Home from './pages/Home'
 import Contact from './pages/Contact'
-import Layout from './components/Layout.jsx';
+import Layout from './components/Layout.jsx'
+import NotFound from './pages/NotFound';
 
 export default function App() {
   return (
     <Routes>
-      <Route
-        path="/"
-        element={
-          <Layout>
-            <Home />
-          </Layout>
-        }
-      />
+      <Route element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="concact" element={<Contact />} />
+      </Route>
 
-      <Route
-        path="/contact"
-        element={
-          <Layout>
-            <Contact />
-          </Layout>
-        }
-      />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   )
 }
